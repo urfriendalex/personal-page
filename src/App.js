b2b62c96b9d9078f1ff36a5d9b98cc968bd27d6a
urfriendalex <alex.yansons@gmail.com>
2020-07-07 14:16:30 +0200

    created layout with the loader animation added custom cursor [WIP] intersection observer scroll animations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {  useRef, useState } from 'react';
+import './styles/App.scss';
+import Loader from './Components/Loader';
+import Landing from './Components/Landing';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const cursor = useRef(null);
+  const [isLoading, setIsloading] = useState(false);
+
+  const handleIsLoading = (status) => {
+    setIsloading(status);
+  };
+
+  const onMove = (e) => {
+    // cursor.current.style.transform = `translate(${e.pageX}px,${e.pageY}px)`;
+    cursor.current.style.left = `${e.pageX}px`;
+    cursor.current.style.top = `${e.pageY}px`;
+  };
+
+  return isLoading ? (
+    <Loader handleIsLoading={handleIsLoading} />
+  ) : (
+    <div className='App' onMouseMove={onMove}>
+      <div ref={cursor} id='cursor'></div> 
+      <Landing />
     </div>
   );
 }
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef } from 'react';
+import { TimelineLite } from 'gsap';
+
+const Loader = ({ handleIsLoading }) => {
+  const tl = new TimelineLite();
+
+  const loader = useRef(null);  
+
+  useEffect(() => {
+    tl.to(loader.current, 0, { css: { visibility: 'visible' } })
+      .fromTo(
+        loader.current.childNodes[0],
+      .5,
+        { left: '-100vw' },
+        { left: '0' }
+      )
+      .fromTo(
+        loader.current.childNodes[1],
+        .5,
+        { right: '-100vw' },
+        { right: '0' }
+      )
+      .to(
+        loader.current.childNodes[1],
+        .5,
+        { backgroundColor: '#2D2D2A' }
+      );
+
+      tl.eventCallback('onComplete', () => {
+        console.log('i am done');
+        handleIsLoading(false);
+      });
+      
+
+    return () => {
+      console.log("i am disappearing");
+    };
+  });
+
+  return(
+    <div ref={loader} className='loader'>
+      <div className='loader loader-first'>That is loader 1</div>
+      <div className='loader loader-second'>That is loader 2</div>
+    </div>
+  );
+};
+
+export default Loader;
